939dc5427d0a5f4516a83754603abf59bdd1017a
Anton Izhboldin <antontixa@gmail.com>
2024-12-18 23:40:34 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import './App.css'
 import Header from "./components/header/header"
 import Footer from './components/footer/footer'
 import { Outlet } from "react-router-dom";
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    localStorage.setItem('nameStore', "Купер");
+  },[])
 
   return (
     <div className='mainPosition'>
