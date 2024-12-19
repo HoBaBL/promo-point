1e507aa50c7e4c6f49848ddcd3b70b38f49e99ef
Anton Izhboldin <antontixa@gmail.com>
2024-12-19 15:08:04 +0300

    маршрутизация

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Header from "./components/header/header"
 import Footer from './components/footer/footer'
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useEffect } from 'react';
 
 function App() {
+  const navigate = useNavigate()
+
   useEffect(() => {
     localStorage.setItem('nameStore', "Купер");
+    navigate('/home')
   },[])
 
   return (
